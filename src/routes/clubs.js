bb6e8882f9eb246785e5428c713202b637ae44ec
RyotaYamamoto990319 <ryou.ta.ya.03@gmail.com>
2021-09-18 22:28:04 +0900

    initialize database

--- src/routes/clubs.js
+++ src/routes/clubs.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('clubs', { title: 'サークル一覧' });
+router.get('/', async function(req, res, next) {
+  const clubs = await db.Club.findAll();
+  clubs.map((club) => {console.log(club.name)});
+  res.render('clubs', { title: 'サークル一覧', clubs:clubs });
 });
 
 module.exports = router;
