376043d664593a2b6d9850840bcbb5187c2586d9
ohashi-y <y17122014@gmail.com>
2021-09-18 14:46:35 +0900

    sign 3

--- src/public/javascripts/sign.js
+++ src/public/javascripts/sign.js
@@ -7,9 +7,18 @@ submit.addEventListener('click', (e) => {
     // 規定の動作をキャンセル
     e.preventDefault();
     // イベント発生時に行う処理
-    console.log(document.getElementById("email").value);
-    console.log(document.getElementById("password").value);
+    email = document.getElementById("email").value;
+    pass = document.getElementById("password").value;
+    rpass = document.getElementById("re_password");
 
+    if(pass != rpass.value){
+        rpass.insertAdjacentHTML("afterend","<br>パスワードが違います</br>");
+        rpass.value = '';
+        return false;
+    }
+
+    console.log(email)
+    console.log(pass)
   })
 
   /*
