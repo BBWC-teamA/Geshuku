84551a095d7c5cf4db713fb8acb285a6dc090714
RyotaYamamoto990319 <ryou.ta.ya.03@gmail.com>
2021-09-19 14:58:14 +0900

    send data from database in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const { Op } = require("sequelize");
+const db = require('../models');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'メイン' });
+const sendNum = 6;
+
+const createRandomArray = (n) => {
+  var array = [];
+  while (array.length < sendNum) {
+    var i = Math.floor( Math.random() * n) + 1;
+    if (array.indexOf(i) < 0) {
+      array.push(i);
+    }
+  }
+  return array;
+}
+
+router.get('/', async function(req, res, next) {
+  db.Club.count().then(dataCount => {
+    var idArr = createRandomArray(dataCount);
+    console.log(idArr);
+    db.Club.findAll({
+      where: {
+        id: {
+          [Op.in]: idArr
+        }
+      }
+    }).then(clubs => {
+      res.render('index', { title: 'メイン', clubs: clubs});
+    })
+  });
 });
 
 module.exports = router;
