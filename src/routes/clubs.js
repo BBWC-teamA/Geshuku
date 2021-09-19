9bce636c9928f07396615aa0b6d46dd0812e0ee5
RyotaYamamoto990319 <ryou.ta.ya.03@gmail.com>
2021-09-19 18:33:08 +0900

    send tagArray

--- src/routes/clubs.js
+++ src/routes/clubs.js
@@ -5,8 +5,18 @@ const db = require('../models');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const clubs = await db.Club.findAll();
-  clubs.map((club) => {console.log(club.name)});
-  res.render('clubs', { title: 'サークル一覧', clubs:clubs });
+  // clubs.map((club) => {console.log(club.name)});
+  db.ClubTag.findAll({
+    attributes: ['tag'],
+  }).then(tags => {
+    var tagArray = []
+    tags.map((tag) => {
+      if (tagArray.indexOf(tag.tag) < 0) {
+        tagArray.push(tag.tag);
+      }
+    });
+    res.render('clubs', { title: 'サークル一覧', clubs:clubs, tags:tagArray});
+  });
 });
 
 module.exports = router;
--- src/routes/edit.js
+++ src/routes/edit.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('edit', { title: '編集ページ' });
+  db.ClubTag.findAll({
+    attributes: ['tag'],
+  }).then(tags => {
+    var tagArray = []
+    tags.map((tag) => {
+      if (tagArray.indexOf(tag.tag) < 0) {
+        tagArray.push(tag.tag);
+      }
+    });
+    console.log(tagArray);
+      res.render('edit', { title: '編集ページ', tags:tagArray});
+  });
 });
 
 module.exports = router;
