eb4b338d9ee42ddf24ff8a99b0cbb103e82d9173
ohashi-y <y17122014@gmail.com>
2021-09-19 22:47:04 +0900

    clubs未完

---
+++ src/public/javascripts/clubs.js
@@ -0,0 +1,18 @@
+//var on_tag = document.getElementById("on_tag");
+//var off_tag = document.getElementById("off_tag");
+
+console.log("aaa")
+
+
+/*const clickHandler = (event) => {
+    //const className = event.target.className;
+    //console.log(className);
+  };*/
+
+  
+/*  return (
+    <div className="parentClassName" onClick={clickHandler}>
+      <p>親要素</p>
+      <div className="childClassName">子要素</div>
+    </div>
+  );*/
