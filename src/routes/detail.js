5557fb30dd3593978e313c7d4015753295540382
RyotaYamamoto990319 <ryou.ta.ya.03@gmail.com>
2021-09-19 15:29:33 +0900

    edit query of detail.js

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('detail', { title: 'サークル詳細' });
+router.get('/:id', async function(req, res, next) {
+  // console.log(req.params.id);
+  const club = await db.Club.findByPk(req.params.id);
+  res.render('detail', { title: 'サークル詳細', club:club});
 });
 
 module.exports = router;
