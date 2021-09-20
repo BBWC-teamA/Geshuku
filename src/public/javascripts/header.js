51456a2f074ba30c3ccc471a98f90c81c8702552
RyotaYamamoto990319 <ryou.ta.ya.03@gmail.com>
2021-09-20 09:53:27 +0900

    change header transition

--- src/public/javascripts/header.js
+++ src/public/javascripts/header.js
@@ -5,6 +5,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
     initMain();
 });
 
+const post = (path, params, method='post') => {
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    const form = document.createElement('form');
+    form.method = method;
+    form.action = path;
+
+    for (const key in params) {
+        if (params.hasOwnProperty(key)) {
+            const hiddenField = document.createElement('input');
+            hiddenField.type = 'hidden';
+            hiddenField.name = key;
+            hiddenField.value = params[key];
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
 const initMain = () => {
 
     getUser(function(user){ 
@@ -24,10 +46,18 @@ const initMain = () => {
                 });
             })
 
+            document.getElementById("returnmain").onclick = () => {
+                post('/', {contact:email});
+            }
+
             document.getElementById("mypage").onclick = () => {
                 window.location.href = '/mypage/' + user.email;
             }
 
+        } else {
+            document.getElementById("returnmain").onclick = () => {
+                window.location.href = '/';
+            }
         }
     });
   
